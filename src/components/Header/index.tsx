baa788a6cb0b920be3417da2cb032d5353b33285
Tyson Le <taichi.le3@gmail.com>
2025-05-29 05:00:41 +1000

    Add Dark Mode Support

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
+import { SunOutlined } from '@ant-design/icons';
+import './index.css';
+import { useEffect, useState } from 'react';
+
 export default function Header() {
+  const [isDark, setIsDark] = useState(false);
+  useEffect(() => {
+    document.body.classList.toggle('dark', isDark);
+  }, [isDark]);
   return (
     <header className="flex justify-between text-center center max-w-200 m-auto pt-1 px-4 pb-2">
       <div>Tyson Le</div>
+      <SunOutlined className="sun-icon" onClick={() => setIsDark(!isDark)} />
     </header>
   );
 }
