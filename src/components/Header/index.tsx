74ad5d4a79e871db83edf22ec98dbe3ee8d6c00e
Tyson Le <taichi.le3@gmail.com>
2025-05-29 05:15:42 +1000

    Bolded name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   }, [isDark]);
   return (
     <header className="flex justify-between text-center center max-w-200 m-auto pt-1 px-4 pb-2">
-      <div>Tyson Le</div>
+      <div className="font-bold">Tyson Le</div>
       <Switch
         unCheckedChildren={<SunOutlined className="sun-icon" />}
         checkedChildren={<SunFilled className="sun-icon" />}
